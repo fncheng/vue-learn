4d5a85d2fcf4e6d16e304f5958754762a6185e9f
fncheng <dong862647669@gmail.com>
2020-07-23 22:08:28 +0800

    update: 引入ant-design-vue

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// 引入ant-design-vue
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
-import User from "./views/User";
-import About from "./views/About";
+import User from './views/User'
+import About from './views/About'
 
-Vue.config.productionTip = false;
-
+Vue.config.productionTip = false
+// 使用ant-design-vue
+Vue.use(Antd)
 // 全局注册组件
-Vue.component("user", User);
-Vue.component("about", About);
+Vue.component('user', User)
+Vue.component('about', About)
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
