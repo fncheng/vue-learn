b04246a4d100bdcc2850f5d73cdb5aec463710e4
fncheng <dong862647669@gmail.com>
2020-08-17 10:47:50 +0800

    组件

---
+++ src/components/componentA.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="component-a">
+    <span>msg: {{ myMsg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "component-a",
+  // props: ["my-msg"],
+  props: {
+    myMsg: String
+  }
+}
+</script>
+
+<style></style>
