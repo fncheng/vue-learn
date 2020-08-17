4f8c67ad996b8f0351733ed391bf5acda70f7306
fncheng <dong862647669@gmail.com>
2020-08-17 10:48:20 +0800

    Add: 具名插槽和作用域插槽父组件

---
+++ src/views/Slot/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="slot">
+    <slot-a>123456插槽内容</slot-a>
+    <slot-a></slot-a>
+    <slot-b>
+      <template v-slot:header>
+        头部123
+      </template>
+      <!-- 作用域插槽 -->
+      <template v-slot="slotProps">
+        {{ slotProps }} <br />
+        {{ slotProps.user }} <br />
+        {{ slotProps.age }}
+      </template>
+
+      <template v-slot:footer>
+        尾部456
+      </template>
+    </slot-b>
+  </div>
+</template>
+
+<script>
+import slotA from "../../components/slotA"
+import slotB from "@/components/slotB"
+export default {
+  data() {
+    return {
+      user: {
+        firstName: "first",
+        lastName: "last"
+      }
+    }
+  },
+  name: "Slot",
+  components: {
+    "slot-a": slotA,
+    "slot-b": slotB
+  }
+}
+</script>
+
+<style></style>
