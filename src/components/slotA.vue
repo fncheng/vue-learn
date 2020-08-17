112925d5edbe5da4699ddc52bc1262eda7558744
fncheng <dong862647669@gmail.com>
2020-08-17 10:46:45 +0800

    Add: 插槽和后备内容

---
+++ src/components/slotA.vue
@@ -0,0 +1,22 @@
+// 插槽和后备内容
+<template>
+  <div class="my-slot-a">
+    <p>这是插槽</p>
+    <slot>这是后备内容</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "slotA"
+}
+</script>
+
+<style scoped>
+.my-slot-a {
+  width: 200px;
+  margin: 0 auto;
+  margin-bottom: 5px;
+  background-color: #ccc;
+}
+</style>
