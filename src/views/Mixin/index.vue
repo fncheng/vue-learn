dd50d1280398f489dfcdba4082a321803cb48809
fncheng <dong862647669@gmail.com>
2020-08-25 23:39:38 +0800

    Add: 组件mixin的使用

---
+++ src/views/Mixin/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mixins">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+// 组件mixin
+const myMixin = {
+  created() {
+    this.hello()
+  },
+  methods: {
+    hello() {
+      console.log(this.msg)
+    }
+  }
+}
+export default {
+  mixins: [myMixin],
+  name: "mixins",
+  data() {
+    return {
+      msg: "mixin"
+    }
+  }
+}
+</script>
+
+<style></style>
