c974971c89ee9522d2c79a5cbc423d2a34ad1d7f
fncheng <dong862647669@gmail.com>
2020-09-04 13:44:47 +0800

    Update: $refs, $el

---
+++ src/components/componentB.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="component-b">
+    <slot>
+      <div>slot中默认内容</div>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "component-b",
+  // props: ["my-msg"],
+  props: {
+    myMsg: String
+  },
+  methods: {
+    getSlot() {
+      console.log("$slots:", this.$slots.default[0].text)
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/views/Father.vue
+++ src/views/Father.vue
@@ -2,16 +2,19 @@
 <template>
   <div class="father">
     <input type="text" v-model="inputValue" />
-    <component-a :my-msg="msg" ref="myComponent"></component-a>
+    <component-a :my-msg="msg" ref="myComponent">123</component-a>
+    <component-b ref="myComponentB">123</component-b>
     <button @click="bindClick">点击</button>
   </div>
 </template>
 
 <script>
 import componentA from "@/components/componentA"
+import componentB from "@/components/componentB"
 export default {
   components: {
-    componentA: componentA
+    componentA: componentA,
+    componentB: componentB
   },
   data() {
     return {
@@ -22,8 +25,15 @@ export default {
   methods: {
     bindClick(props) {
       console.log(props)
-      console.log(this.$refs.myComponent)
+      // console.log(this.$refs.myComponent)
+      // console.log("Father B: ", this.$el)
+      // console.log("B: ", this.$refs["myComponentB"].$el.innerText)
+      // console.log("$slot: ", this.$slots)
+      this.$refs.myComponentB.getSlot()
     }
+  },
+  mounted() {
+    console.log("组件：", this.$children)
   }
 }
 </script>
