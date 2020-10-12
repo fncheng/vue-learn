b3b16813fc888c897eac41a9925d7d8a209bdd14
fncheng <dong862647669@gmail.com>
2020-10-12 15:52:30 +0800

    Add: Element-UI

---
+++ src/views/ElementUI/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="element-ui">
+    <template>
+      <el-button type="text" @click="open">点击打开 Message Box</el-button>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    open() {
+      this.$alert('', '标题名称', {
+        confirmButtonText: '确定',
+        center: true
+      })
+    }
+  }
+}
+</script>
+
+<style></style>
