16f81f858c23f8f57b94aea767920f6605e32c05
fncheng <dong862647669@gmail.com>
2020-10-19 17:56:54 +0800

    生命周期

---
+++ src/views/LifeCycle/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="life">
+    life
+  </div>
+</template>
+
+<script>
+export default {
+  beforeCreate: function() {
+    console.group('------beforeCreate创建前状态------')
+    console.log('%c%s', 'color:red', 'el     : ' + this.$el) //undefined
+    console.log('%c%s', 'color:red', 'data   : ' + this.$data) //undefined
+    console.log('%c%s', 'color:red', 'message: ' + this.message)
+  },
+  created: function() {
+    console.group('------created创建完毕状态------')
+    console.log('%c%s', 'color:red', 'el     : ' + this.$el) //undefined
+    console.log('%c%s', 'color:red', 'data   : ' + this.$data) //已被初始化
+    console.log('%c%s', 'color:red', 'message: ' + this.message) //已被初始化
+  },
+  beforeMount: function() {
+    console.group('------beforeMount挂载前状态------')
+    console.log('%c%s', 'color:red', 'el     : ' + this.$el) //已被初始化
+    console.log(this.$el)
+    console.log('%c%s', 'color:red', 'data   : ' + this.$data) //已被初始化
+    console.log('%c%s', 'color:red', 'message: ' + this.message) //已被初始化
+  },
+  mounted: async function() {
+    let res = await this.getNumber()
+    console.log(res)
+    console.group('------mounted 挂载结束状态------')
+    console.log('%c%s', 'color:red', 'el     : ' + this.$el) //已被初始化
+    console.log(this.$el)
+    console.log('%c%s', 'color:red', 'data   : ' + this.$data) //已被初始化
+    console.log('%c%s', 'color:red', 'message: ' + this.message) //已被初始化
+  },
+  beforeUpdate: function() {
+    console.group('beforeUpdate 更新前状态===============》')
+    console.log('%c%s', 'color:red', 'el     : ' + this.$el)
+    console.log(this.$el)
+    console.log('%c%s', 'color:red', 'data   : ' + this.$data)
+    console.log('%c%s', 'color:red', 'message: ' + this.message)
+  },
+  updated: function() {
+    console.group('updated 更新完成状态===============》')
+    console.log('%c%s', 'color:red', 'el     : ' + this.$el)
+    console.log(this.$el)
+    console.log('%c%s', 'color:red', 'data   : ' + this.$data)
+    console.log('%c%s', 'color:red', 'message: ' + this.message)
+  },
+  beforeDestroy: function() {
+    console.group('beforeDestroy 销毁前状态===============》')
+    console.log('%c%s', 'color:red', 'el     : ' + this.$el)
+    console.log(this.$el)
+    console.log('%c%s', 'color:red', 'data   : ' + this.$data)
+    console.log('%c%s', 'color:red', 'message: ' + this.message)
+  },
+  destroyed: function() {
+    console.group('destroyed 销毁完成状态===============》')
+    console.log('%c%s', 'color:red', 'el     : ' + this.$el)
+    console.log(this.$el)
+    console.log('%c%s', 'color:red', 'data   : ' + this.$data)
+    console.log('%c%s', 'color:red', 'message: ' + this.message)
+  },
+  methods: {
+    getNumber() {
+      return setTimeout(() => {
+        console.log(123)
+      }, 3000)
+    }
+  }
+}
+</script>
+
+<style></style>
