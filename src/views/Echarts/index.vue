b4b3515b64a77e4f9e2d4f28ba83f1f07643455f
fncheng <dong862647669@gmail.com>
2020-11-10 17:49:01 +0800

    Echarts

---
+++ src/views/Echarts/index.vue
@@ -0,0 +1,196 @@
+<template>
+  <div>
+    <div id="main"></div>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  name: 'echartsmorey',
+  data() {
+    return {
+      list: [
+        {
+          x: '01:00',
+          y: 1,
+          z: 9,
+          x2: '1.1'
+        },
+        {
+          x: '03:00',
+          y: 2,
+          z: 70,
+          x2: '2.1'
+        },
+        {
+          x: '05:00',
+          y: 3,
+          z: 1,
+          x2: '3.1'
+        },
+        {
+          x: '07:00',
+          y: 1,
+          z: 20,
+          x2: '4.1'
+        },
+        {
+          x: '09:00',
+          y: 0,
+          z: 18,
+          x2: '5.1'
+        },
+        {
+          x: '11:00',
+          y: 10,
+          z: 12,
+          x2: '6.1'
+        },
+        {
+          x: '13:00',
+          y: 10,
+          z: 12,
+          k: 11
+        },
+        {
+          x: '15:00',
+          y: 10,
+          z: 12,
+          x2: '7.1'
+        },
+        {
+          x: '17:00',
+          y: 10,
+          z: 12,
+          x2: '8.1'
+        }
+      ],
+      dayX: [], // 当天的 X轴
+      monthX: [], // 当月的 X轴
+      dataY: [] // Y 轴
+    }
+  },
+  methods: {
+    init(dataX, dataY) {
+      this.myChart = echarts.init(document.getElementById('main'))
+      let option = {
+        // 图例组件
+        legend: {
+          icon: 'stack',
+          data: ['当天', '当月', '当年'],
+          selectedMode: 'single', // 单选
+          selected: {
+            当天: false,
+            当月: true,
+            当年: false
+          }
+        },
+        tooltip: {
+          trigger: 'none',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        // 工具栏
+        toolbox: {
+          x: 1100,
+          y: 0,
+          feature: {
+            saveAsImage: {
+              name: `test`
+            }
+          }
+        },
+        grid: {
+          left: '5%', //组件距离容器左边的距离
+          right: '20%',
+          top: '15%'
+        },
+        // X轴 滑块 可缩放
+        dataZoom: [
+          {
+            type: 'slider',
+            show: true,
+            start: 0, // 开始百分数
+            end: 100 // 结束百分数
+          }
+        ],
+        xAxis: {
+          type: 'category',
+          // X 轴分隔线样式
+          splitLine: {
+            show: true,
+            lineStyle: {
+              color: ['#f3f0f0'],
+              width: 2,
+              type: 'dotted'
+            }
+          },
+          // x轴数据
+          data: dataX
+        },
+        yAxis: [
+          {
+            name: 'Y轴单位写的位置',
+            type: 'value',
+            position: 'left',
+            splitNumber: 10, // Y 轴分隔格数
+            // Y 轴分隔线样式
+            splitLine: {
+              show: true,
+              lineStyle: {
+                color: ['#f3f0f0'],
+                width: 2,
+                type: 'solid'
+              }
+            }
+          }
+        ],
+        series: dataY
+      }
+
+      this.myChart.on('legendselectchanged', obj => {
+        var options = this.myChart.getOption()
+        options.xAxis[0].data = obj.name == '当月' ? this.monthX : this.dayX
+        this.myChart.setOption(options, true)
+      })
+
+      // 使用刚指定的配置项和数据显示图表。
+      this.myChart.setOption(option)
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.monthX = this.list.map(item => item.x2)
+      this.dayX = this.list.map(item => item.x)
+
+      this.dataY.push({
+        name: '当月',
+        type: 'line', // 直线
+        data: this.list.map(item => item.y)
+      })
+
+      this.dataY.push({
+        name: '当天',
+        type: 'line',
+        data: this.list.map(item => item.z)
+      })
+      this.dataY.push({
+        name: '当年',
+        type: 'line',
+        data: this.list.map(item => item.z)
+      })
+
+      this.init(this.dayX, this.dataY)
+    })
+  }
+}
+</script>
+
+<style scoped>
+#main {
+  width: 1200px;
+  height: 600px;
+}
+</style>
---
+++ src/views/Echarts/index2.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div id="main"></div>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  name: 'echarts2',
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+    const myChart = echarts.init(document.getElementById('main'))
+    let option = {
+      xAxis: {
+        type: 'category',
+        data: ['11-1', '11-2', '11-3', '11-4', '11-5', '11-6']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'totalmen',
+          data: [120, 332, 901, 400, 600, 800],
+          type: 'line',
+          smooth: true,
+          lineStyle: {
+            color: {
+              type: 'linear',
+              x: 0,
+              x2: 1,
+              y: 0,
+              y2: 0,
+              colorStops: [
+                {
+                  offset: 0,
+                  color: '#7DAFFF'
+                },
+                {
+                  offset: 1,
+                  color: '#4595FF'
+                }
+              ]
+            },
+            width: 3
+          }
+        },
+        {
+          name: 'total',
+          data: [220, 332, 680, 400, 780, 432],
+          type: 'line',
+          smooth: true,
+          // 线条样式
+          lineStyle: {
+            color: {
+              type: 'linear',
+              x: 0,
+              x2: 1,
+              y: 0,
+              y2: 0,
+              colorStops: [
+                {
+                  offset: 0,
+                  color: '#82E087'
+                },
+                {
+                  offset: 1,
+                  color: '#78C97D'
+                }
+              ]
+            },
+            width: 3
+          }
+        }
+      ],
+      grid: {
+        top: 20,
+        right: 60,
+        bottom: 20,
+        left: 40
+      }
+    }
+    myChart.setOption(option)
+    console.log(myChart)
+  }
+}
+</script>
+
+<style scoped>
+#main {
+  width: 1200px;
+  height: 600px;
+}
+</style>
