58fb7cd4e27a16eb840aafb3714f908eb07342d7
cheng <dong862647669@gmail.com>
2021-01-07 17:40:57 +0800

    封装aplyer组件

---
+++ src/common/aplayer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="aplayer"></div>
+</template>
+
+<script>
+import 'aplayer/dist/APlayer.min.css'
+import APlayer from 'aplayer'
+export default {
+  data() {
+    return {}
+  },
+  props: {
+    options: {
+      type: Object,
+      require: true,
+      container: {
+        type: String,
+        require: true
+      },
+      fixed: {
+        type: Boolean,
+        default: false
+      }, //开启吸底模式,
+      mini: {
+        type: Boolean,
+        default: false
+      }, //开启迷你模式
+      autoplay: {
+        type: Boolean,
+        default: false
+      }, // 自动播放
+      theme: {
+        type: String,
+        default: '#b7daff'
+      } //主题色
+    }
+  },
+  mounted() {
+    this.options.container = document.getElementById('aplayer')
+    new APlayer(this.options)
+  }
+}
+</script>
+
+<style></style>
