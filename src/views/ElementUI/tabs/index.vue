612dace1b8dd9fec6809008137900b6703bac43c
fncheng <dong862647669@gmail.com>
2021-03-22 14:59:05 +0800

    Update tabs

--- src/views/ElementUI/tabs/index.vue
+++ src/views/ElementUI/tabs/index.vue
@@ -12,6 +12,10 @@
         >定时任务补偿</el-tab-pane
       >
     </el-tabs>
+    <div>{{ dataName }}</div>
+    <div>
+      <div v-for="item in list" :key="item">{{ item }}</div>
+    </div>
   </div>
 </template>
 
@@ -19,7 +23,23 @@
 export default {
   data() {
     return {
-      activeName: 'first'
+      activeName: 'first',
+      dataName: 'this is data name',
+      list: [1, 2, 3, 4, 5]
+    }
+  },
+  computed: {
+    // dataName() {
+    //   return this.activeName
+    // }
+  },
+  watch: {
+    dataName() {
+      if (this.dataName == '1') {
+        console.log('dataName has changed')
+      }
+      console.log('val has changed')
+      // return this.activeName
     }
   },
   methods: {
