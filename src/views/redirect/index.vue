e6c2d66451355da833878f6c4569ec03a6971ba0
fncheng <dong862647669@gmail.com>
2021-03-22 15:01:06 +0800

    Add Redirect

---
+++ src/views/redirect/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="redirect">Redirect</div>
+</template>
+
+<script>
+export default {
+  // created() {
+  //   const { params, query } = this.$route
+  //   const { path } = params
+  //   this.$router.replace({ path: '/' + path, query })
+  // },
+  beforeCreate() {
+    console.log('this.$route', this.$route)
+    const { query } = this.$route
+    const path = query.path
+    // console.log('path:', path, this.$route)
+    // 跳转至path页面
+    this.$router.replace({ path: path })
+  },
+  mounted() {},
+  render: function(h) {
+    return h() // avoid warning message
+  }
+}
+</script>
+
+<style></style>
