49b1a4b0542a9e2e7aaa72ced8b83ba3618e886f
fncheng <dong862647669@gmail.com>
2021-05-19 17:20:00 +0800

    浏览器title动态修改

--- src/mixins.js
+++ src/mixins.js
@@ -1,6 +1,24 @@
-const mixin = {
-  created() {
-    console.log('global mixin created')
+/**
+ * 获取title
+ * @param {String} vm VueComponent实例
+ * @returns $route.meata.title
+ */
+const getTitle = (vm) => {
+  const { title } = vm.$options
+  if (title) {
+    return typeof title === 'function' ? title.call(vm) : title
+  } else {
+    return vm.$route?.meta?.title || ''
+  }
+}
+
+const globalMixin = {
+  mounted() {
+    // 浏览器title动态修改
+    const title = getTitle(this)
+    if (title) {
+      document.title = title
+    }
   },
 }
-export default mixin
+export default globalMixin
